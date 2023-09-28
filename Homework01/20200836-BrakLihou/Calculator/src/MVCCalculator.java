7e668e5cad038302b88fc946660175b743d21ad6
Lihoubrak <blh.braklihou18@gmail.comgit config --global user.email blh.braklihou18@gmail.comgit config --global user.email blh.braklihou18@gmail.com>
2023-09-29 03:06:22 +0700

    lihou push

--- Homework01/20200836-BrakLihou/Calculator/src/MVCCalculator.java
+++ Homework01/20200836-BrakLihou/Calculator/src/MVCCalculator.java
@@ -2,10 +2,20 @@ import controller.CalculatorController;
 import model.CalculatorModel;
 import view.Calculator;
 
+import java.util.Scanner;
+
 public class MVCCalculator {
     public static void main(String[] args) {
         CalculatorModel model = new CalculatorModel();
         Calculator view = new Calculator();
         CalculatorController controller = new CalculatorController(view, model);
+
+
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Nhập tên của bạn: ");
+        String name = scanner.nextLine();
+        scanner.close();
+        System.out.println("Xin chào " + name + "!");
     }
 }
